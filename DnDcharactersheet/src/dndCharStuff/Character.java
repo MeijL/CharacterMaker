e98ae0ed2a6a1d1987c174ae1b8b10665d9d9226
Meij <37117513+MeijL@users.noreply.github.com>
2018-05-16 12:26:38 -0500

    Update Character.java

--- DnDcharactersheet/src/dndCharStuff/Character.java
+++ DnDcharactersheet/src/dndCharStuff/Character.java
@@ -1,5 +1,36 @@
 package dndCharStuff;
 
 public class Character {
+  
+  	private String name;
+	private int level;
+	
+	// Constructors for character
+	
+	public Character() {
+		name = "Generic Hero";
+		level = 0;
+	}
+	
+	public Character(String n, int l) {
+		name = n;
+		level = l;
+	}
+	
+  // increment for the level
+  
+	public void incrementLevel() {
+		level++;
+	}
+  
+  // getters
+	
+	public void getName() {
+		return name;
+	}
+	
+	public void getLevel() {
+		return level;
+	}
 
 }
