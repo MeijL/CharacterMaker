299db6fc2a2fa475a96f9dd450c9a9eecafc1e39
Meij <37117513+MeijL@users.noreply.github.com>
2018-05-16 16:12:46 -0500

    Add files via upload

---
+++ DnDcharactersheet/src/dndCharStuff/CharClasses.java
@@ -0,0 +1,32 @@
+package DnDcharactersheet;
+
+import java.util.List;
+
+public class CharClasses {
+	
+	List<String> classes;
+	
+	// Constructor to initialize the list that will contain names of all classes
+	
+	public CharClasses() {
+		classes = new ArrayList<String>();
+	}
+	
+	// Method to add class
+	
+	public void addClass(String thing) {
+		classes.add(thing);
+	}
+	
+	// Method to get class
+	
+	public void getClass(String thing) {
+		for (int i = 0; i < classes.size() - 1; i++) {
+			if (thing.equals(classes.get(i))) {
+				return classes.get(i);
+			}
+		}
+		return "No class found.";
+	}
+
+}
