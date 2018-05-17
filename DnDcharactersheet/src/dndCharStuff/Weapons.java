0239fe9fd9144a99d2568fe35609b17285acba8d
Meij <37117513+MeijL@users.noreply.github.com>
2018-05-17 11:44:07 -0500

    Add files via upload

---
+++ DnDcharactersheet/src/dndCharStuff/Weapons.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class Weapons {
+	
+	private String name;
+	private int modifier;
+	private String weaponType;
+	private String weaponDescription;
+	List<String> weaponList = new ArrayList<String>();
+	
+	public Weapons() {
+		name = "generic weapon";
+		modifier = 0;
+		weaponType = "generic type";
+		weaponDescription = "does nothing, deal with it";
+	}
+	
+	public Weapons(String n, int m, String w, String d) {
+		name = n;
+		modifier = m;
+		weaponType = w;
+		weaponDescription = d;
+		addWeapons(n, m, w);
+		
+	}
+	
+	public void addWeapons(String n, int m, String w) {
+		String weapon = "name: " + n + ", modifier: " + m + ", weapon: " + w;
+		weaponList.add(weapon);
+	}
+
+}
