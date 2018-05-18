41c1a4cbfee381a1c012f6277f0520e69114a7dc
Meij <37117513+MeijL@users.noreply.github.com>
2018-05-18 12:48:01 -0500

    Update Character.java

--- DnDcharactersheet/src/dndCharStuff/Character.java
+++ DnDcharactersheet/src/dndCharStuff/Character.java
@@ -4,17 +4,20 @@ public class Character {
   
   	private String name;
 	private int level;
+	private int exp;
 	
 	// Constructors for character
 	
 	public Character() {
 		name = "Generic Hero";
 		level = 0;
+		exp = 0;
 	}
 	
 	public Character(String n, int l) {
 		name = n;
 		level = l;
+		exp = 0;
 	}
 	
   // increment for the level
@@ -22,6 +25,19 @@ public class Character {
 	public void incrementLevel() {
 		level++;
 	}
+	
+  // ability to add a certain amount of exp to exp
+	
+	public void increaseEXP(int value) {
+		exp += value;
+	}
+	
+   // reset the exp when a level up has occurred
+	
+	public void setToZero() {
+		exp = 0;
+	}
+	
   
   // getters
 	
@@ -31,6 +47,10 @@ public class Character {
 	
 	public void getLevel() {
 		return level;
+	}
+	
+	public void getEXP() {
+		return exp;
 	}
 
 }
