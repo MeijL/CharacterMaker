5c16ce7b51e0d17534c3ba875c1020115c8425b3
Meij <37117513+MeijL@users.noreply.github.com>
2018-05-21 07:38:24 -0500

    Update and rename Weapons.java to Weapon.java

---
+++ DnDcharactersheet/src/dndCharStuff/Weapon.java
@@ -0,0 +1,28 @@
+public class Weapon extends InvItem{
+
+	private String type;
+	private String weaponClass;	
+	private int modifier;
+	private int magicId;
+
+	public Weapon(String t, String w, int m, int id) {
+		type = t;
+		weaponClass = w;
+		modifier = m;
+		magicId = id;
+	}
+	
+	public String getType(){
+		return type;
+	}
+	public String getClass(){
+		return weaponClass;
+	}
+	public int getModifier(){
+		return modifier;
+	}
+	public int getMagic(){
+		return magicId;
+	}
+
+}
