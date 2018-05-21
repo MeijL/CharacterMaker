6085b406c9e5395e2e40527e65fa9a051b881652
Meij <37117513+MeijL@users.noreply.github.com>
2018-05-21 07:34:08 -0500

    Update and rename CharClasses.java to CharClass.java

---
+++ DnDcharactersheet/src/dndCharStuff/CharClass.java
@@ -0,0 +1,32 @@
+package DnDcharactersheet;
+
+import java.util.List;
+
+public class CharClass {
+	
+	String name;
+	int level;
+	List<String> skills;
+	
+// Constructor to initialize the class
+	
+	public CharClass(String n) {
+		name = n;
+	}
+	
+// Method to add class
+	
+	public void addClass(String thing) {
+		
+	}
+	
+// Method to get class
+	
+	public void getClass(String thing) {
+	
+	
+	public void addSkills(String thing) {
+		skills.add(thing);
+	}
+
+}
