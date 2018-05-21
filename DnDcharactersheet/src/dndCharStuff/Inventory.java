ec2eb7104a6594aa6856a1f9d6fba6135acf5a49
Meij <37117513+MeijL@users.noreply.github.com>
2018-05-21 07:37:50 -0500

    Update Inventory.java

--- DnDcharactersheet/src/dndCharStuff/Inventory.java
+++ DnDcharactersheet/src/dndCharStuff/Inventory.java
@@ -1,19 +1,27 @@
 package dndCharStuff;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class Inventory {
 	
-	private ArrayList<String> items;
+	private List<String> items;
 	
 	public Inventory(){
-		items = new ArrayList<>();
+		items = new List<>();
 	}
 	
-	public void addItem(String thing){
+	public void addItem(InvItem thing){
 		items.add(thing);
 	}
-	public void removeItem(String thing){
+	public void removeItem(InvItem thing){
 		items.remove(thing);
+	}
+	public boolean hasItem(InvItem thing) {
+		for (int i = 0; i < items.size() - 1; i++) {
+			if (thing.equals(items.get(i))) {
+				return true
+			}
+		}
+		return false;
 	}
 }
