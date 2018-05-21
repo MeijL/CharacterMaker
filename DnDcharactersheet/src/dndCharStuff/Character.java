52009e71e8586a7fd0ec083cdb99d941176e5c0d
Meij <37117513+MeijL@users.noreply.github.com>
2018-05-21 07:36:58 -0500

    Update Character.java

--- DnDcharactersheet/src/dndCharStuff/Character.java
+++ DnDcharactersheet/src/dndCharStuff/Character.java
@@ -1,14 +1,39 @@
 package dndCharStuff;
 
 public class Character {
-	
-	private CharStats stats;
-	private CStatus status;
+  
+  	private String name;
+	private CharRace race;
+	private CharStatus status;
+	private CharValues values;
 	private Inventory inv;
 	
-	public Character(CharStats c, int h){
-		stats = c;
-		status = new CStatus(h);
-		inv = new Inventory();
+	// Constructors for character
+	
+	public Character() {
+		name = "Generic Hero";
+		level = 0;
 	}
+	
+	public Character(String n, int l) {
+		name = n;
+		level = l;
+	}
+	
+  // increment for the level
+  
+	public void incrementLevel() {
+		level++;
+	}
+  
+  // getters
+	
+	public void getName() {
+		return name;
+	}
+	
+	public void getLevel() {
+		return level;
+	}
+
 }
