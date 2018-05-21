394e1b39aceeab5a05b095c0c78e0cfdb5e33f1b
Meij <37117513+MeijL@users.noreply.github.com>
2018-05-21 07:31:33 -0500

    Add files via upload

---
+++ DnDcharactersheet/src/dndCharStuff/CharRace.java
@@ -0,0 +1,24 @@
+package notes;
+
+public class CharRace {
+		
+		private String name;
+		
+		private int strMod;
+		private int conMod;
+		private int intMod;
+		private int wisMod;
+		private int chaMod;
+		private int dexMod;
+		
+		public CharRace(String n){
+			name = n;
+			strMod = 0;
+			conMod = 0;
+			intMod = 0;
+			wisMod = 0;
+			chaMod = 0;
+			dexMod = 0;
+		}
+
+}
