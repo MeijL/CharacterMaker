915f2e3fe3ed76f2567a07f612d3ec4be74cc5c7
Duncan <37115646+90307835@users.noreply.github.com>
2018-05-22 09:31:57 -0500

    Add files via upload

---
+++ src/Inventory.java
@@ -0,0 +1,26 @@
+
+import java.util.List;
+
+public class Inventory {
+	
+	private List<InvItem> items;
+	
+	public Inventory(){
+		items = new List<>();
+	}
+	
+	public void addItem(InvItem thing){
+		items.add(thing);
+	}
+	public void removeItem(InvItem thing){
+		items.remove(thing);
+	}
+	public boolean hasItem(InvItem thing) {
+		for (int i = 0; i < items.size() - 1; i++) {
+			if (thing.equals(items.get(i))) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
