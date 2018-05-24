185273ae46bda8c210d8c70cde72e0e6b32b4b2e
Duncan <37115646+90307835@users.noreply.github.com>
2018-05-24 08:46:02 -0500

    Add files via upload

---
+++ src/GUIMain.java
@@ -0,0 +1,60 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTabbedPane;
+
+public class GUIMain extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIMain frame = new GUIMain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUIMain() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 500, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JLabel lblTitleAndVersion = new JLabel("DnD 3.5 Character Manager v1.0");
+		contentPane.add(lblTitleAndVersion);
+		
+		JButton btnNewCharacter = new JButton("New Character");
+		btnNewCharacter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.add(btnNewCharacter);
+		
+		JButton btnLoadCharacter = new JButton("Load Character");
+		contentPane.add(btnLoadCharacter);
+	}
+
+}
