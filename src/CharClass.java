9fa7298b561285be097ca080943a8baab9100b7a
Meij <37117513+MeijL@users.noreply.github.com>
2018-05-23 21:23:33 -0500

    Update CharClass.java

--- src/CharClass.java
+++ src/CharClass.java
@@ -30,15 +30,18 @@ public class CharClass {
 // Methods to add or remove skills
 	
 	public void addSkill(String thing) {
+		thing.toLowerCase();
 		skills.add(thing);
 	}
 	public void removeSkill(String thing){
+		thing.toLowerCase();
 		skills.remove(thing);
 	}
 	
 // Method to get skills
 	
 	public String getSkill(String thing) {
+		thing.toLowerCase();
 		for (int i = 0; i <= skills.size(); i++) {
 			if (thing.equals(skills.get(i))) {
 				return skills.get(i);
