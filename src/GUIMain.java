b00fd21bc8bfdc05d4c3c2f96a89266cf2eb0b4c
Meij <37117513+MeijL@users.noreply.github.com>
2018-05-30 11:25:59 -0500

    Update GUIMain.java

--- src/GUIMain.java
+++ src/GUIMain.java
@@ -49,23 +49,65 @@ public class GUIMain extends JFrame {
 				panel.add(charMaker);
 				JTextField charName = new JTextField(20);
 				panel.add(charName);
-				Character name = new Character(charName.getText());
 				JButton enter = new JButton("Enter");
 				panel.add(enter);
 				enter.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
+						Character name = new Character(charName.getText());
+						GridLayout raceLayout = new GridLayout(0,2);
 						JFrame raceFrame = new JFrame(charName.getText() + "'s Race");
 						raceFrame.setVisible(true);
-						raceFrame.setSize(500, 400);
+						raceFrame.setSize(500, 500);
+						raceFrame.setLayout(raceLayout);
 						JPanel racePanel = new JPanel();
 						raceFrame.add(racePanel);
-						JLabel charRace = new JLabel("Character race:");
+						JLabel charRace = new JLabel("Character Race: ");
 						racePanel.add(charRace);
-						JTextField textRace = new JTextField(20);
+						JTextField textRace = new JTextField(5);
 						racePanel.add(textRace);
-						CharRace race = new CharRace(textRace.getText());
+						JLabel charSTR = new JLabel("Strength Mod: ");
+						racePanel.add(charSTR);
+						JTextField textSTR = new JTextField(5);
+						racePanel.add(textSTR);
+						JLabel charINT = new JLabel("Intelligence Mod: ");
+						racePanel.add(charINT);
+						JTextField textINT = new JTextField(5);
+						racePanel.add(textINT);
+						JLabel charCON = new JLabel("Constitution Mod: ");
+						racePanel.add(charCON);
+						JTextField textCON = new JTextField(5);
+						racePanel.add(textCON);
+						JLabel charWIS = new JLabel("Wisdom Mod: ");
+						racePanel.add(charWIS);
+						JTextField textWIS = new JTextField(5);
+						racePanel.add(textWIS);
+						JLabel charDEX = new JLabel("Dexterity Mod: ");
+						racePanel.add(charDEX);
+						JTextField textDEX = new JTextField(5);
+						racePanel.add(textDEX);
+						JLabel charCHA = new JLabel("Charisma Mod: ");
+						racePanel.add(charCHA);
+						JTextField textCHA = new JTextField(5);
+						racePanel.add(textCHA);
 						JButton raceEnter = new JButton("Enter");
 						racePanel.add(raceEnter);
+						raceEnter.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								CharRace race = new CharRace(textRace.getText());
+								race.setChaMod(Integer.parseInt(textCHA.getText()));
+								race.setWisMod(Integer.parseInt(textWIS.getText()));
+								race.setIntMod(Integer.parseInt(textINT.getText()));
+								race.setConMod(Integer.parseInt(textCON.getText()));
+								race.setDexMod(Integer.parseInt(textDEX.getText()));
+								race.setStrMod(Integer.parseInt(textSTR.getText()));
+								GridLayout classLayout = new GridLayout(0,2);
+								JFrame classFrame = new JFrame(charName.getText() 
+										+ "'s Class");
+								classFrame.setVisible(true);
+								classFrame.setSize(500, 400);
+								classFrame.setLayout(classLayout);
+							}
+						});
 					}
 				});
 			}
